36165cc2da8815b11861bb350db07723ae18893c
Almir Sarajčić <almir@optimum.ba>
2016-10-26 13:35:36 +0200

    Move fetching to CurrencyLayer.Request module

--- lib/currency_layer.ex
+++ lib/currency_layer.ex
@@ -1,6 +1,8 @@
 defmodule CurrencyLayer do
+  alias CurrencyLayer.Request
+
   def fetch_live_rates() do
-    json = fetch_json("live")
+    json = Request.fetch("live")
     {:ok, map} = JSON.decode(json)
     %{"success" => true, "timestamp" => timestamp, "quotes" => quotes} = map
     rates = Enum.into(quotes, %{}, fn({key, value}) ->
@@ -17,7 +19,7 @@ defmodule CurrencyLayer do
   end
 
   def fetch_historical_rates(date) do
-    json = fetch_json("historical", [date: date])
+    json = Request.fetch("historical", [date: date])
     {:ok, map} = JSON.decode(json)
     %{"success" => true, "timestamp" => timestamp, "quotes" => quotes} = map
     rates = Enum.into(quotes, %{}, fn({key, value}) ->
@@ -34,32 +36,9 @@ defmodule CurrencyLayer do
   end
 
   def fetch_currencies() do
-    json = fetch_json("list")
+    json = Request.fetch("list")
     {:ok, map} = JSON.decode(json)
     %{"success" => true, "currencies" => currencies} = map
     {:ok, currencies}
-  end
-
-  @http Application.get_env(:currency_watch, :currency_layer)[:http_client] || :httpc
-  defp fetch_json(endpoint, params \\ []) do
-    url = endpoint
-    |> create_url()
-    |> add_access_key()
-    |> add_params(params)
-    |> String.to_char_list()
-
-    {:ok, {_, _, body}} = @http.request(url)
-    body
-  end
-
-  @protocol Application.get_env(:currency_watch, :currency_layer)[:protocol] || "http"
-  defp create_url(endpoint), do: @protocol <> "://www.apilayer.net/api/" <> endpoint
-
-  @access_key Application.get_env(:currency_watch, :currency_layer)[:access_key]
-  defp add_access_key(url), do: url <> "?access_key=#{@access_key}"
-
-  defp add_params(url, []), do: url
-  defp add_params(url, [{key, value}|tail]) do
-    add_params(url <> "&#{key}=#{value}", tail)
   end
 end
---
+++ lib/currency_layer/request.ex
@@ -0,0 +1,26 @@
+defmodule CurrencyLayer.Request do
+  @http Application.get_env(:currency_watch, :currency_layer)[:http_client] || :httpc
+  def fetch(endpoint, params \\ []) do
+    url = set_url(endpoint, params)
+    {:ok, {_, _, body}} = @http.request(url)
+    body
+  end
+
+  def set_url(endpoint, params) do
+    endpoint
+    |> create_url()
+    |> add_access_key()
+    |> add_params(params)
+  end
+
+  @protocol Application.get_env(:currency_watch, :currency_layer)[:protocol] || "http"
+  defp create_url(endpoint), do: @protocol <> "://www.apilayer.net/api/" <> endpoint
+
+  @access_key Application.get_env(:currency_watch, :currency_layer)[:access_key]
+  defp add_access_key(url), do: url <> "?access_key=#{@access_key}"
+
+  defp add_params(url, []), do: url
+  defp add_params(url, [{key, value}|tail]) do
+    add_params(url <> "&#{key}=#{value}", tail)
+  end
+end
