f38282f142d9be8f636056e23422fc0384ed3af4
Almir Sarajčić <almir@optimum.ba>
2016-10-26 13:56:39 +0200

    Remove www from CurrencyLayer URLs

--- lib/currency_layer/request.ex
+++ lib/currency_layer/request.ex
@@ -14,7 +14,7 @@ defmodule CurrencyLayer.Request do
   end
 
   @protocol Application.get_env(:currency_watch, :currency_layer)[:protocol] || "http"
-  defp create_url(endpoint), do: @protocol <> "://www.apilayer.net/api/" <> endpoint
+  defp create_url(endpoint), do: @protocol <> "://apilayer.net/api/" <> endpoint
 
   @access_key Application.get_env(:currency_watch, :currency_layer)[:access_key]
   defp add_access_key(url), do: url <> "?access_key=#{@access_key}"
