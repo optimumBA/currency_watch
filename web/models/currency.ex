64604632260e12877ce71141f8ecdfa72bd1873f
Almir Sarajčić <almir.sarajcic@icloud.com>
2017-03-23 17:09:54 +0100

    Add Currency.exists function

--- web/models/currency.ex
+++ web/models/currency.ex
@@ -1,6 +1,8 @@
 defmodule CurrencyWatch.Currency do
   use CurrencyWatch.Web, :model
 
+  alias CurrencyWatch.{Repo, Currency}
+
   schema "currencies" do
     field :code, :string
     field :name, :string
@@ -19,5 +21,14 @@ defmodule CurrencyWatch.Currency do
     |> validate_length(:code, is: 3)
     |> validate_format(:code, ~r/^[A-Z]+$/)
     |> unique_constraint(:code)
+  end
+
+  def exists(code) do
+    query = from(c in Currency, select: 1, limit: 1, where: c.code == ^code)
+
+    case Repo.all(query) do
+      [1] -> true
+      [] -> false
+    end
   end
 end
