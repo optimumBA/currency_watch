71ab0e6d1e5c4e748d3b1a6de788450d1b4cf7ef
Almir Sarajčić <almir.sarajcic@icloud.com>
2017-06-25 13:43:31 +0200

    Add CurrencyLayer.Request.Params module

--- lib/currency_layer.ex
+++ lib/currency_layer.ex
@@ -1,8 +1,10 @@
 defmodule CurrencyLayer do
   alias CurrencyLayer.Request
+  alias CurrencyLayer.Request.Params
 
   def fetch_live_rates() do
-    json = Request.fetch("live")
+    params = %Params{}
+    json = Request.fetch("live", params)
     {:ok, map} = JSON.decode(json)
     %{"success" => true, "timestamp" => timestamp, "quotes" => quotes} = map
     rates = CurrencyLayer.Rate.extract_rates(quotes)
@@ -10,7 +12,8 @@ defmodule CurrencyLayer do
   end
 
   def fetch_historical_rates(date) do
-    json = Request.fetch("historical", [date: date])
+    params = %Params{date: date}
+    json = Request.fetch("historical", params)
     {:ok, map} = JSON.decode(json)
     %{"success" => true, "timestamp" => timestamp, "quotes" => quotes} = map
     rates = Enum.into(quotes, %{}, fn({key, value}) ->
@@ -27,7 +30,8 @@ defmodule CurrencyLayer do
   end
 
   def fetch_currencies() do
-    json = Request.fetch("list")
+    params = %Params{}
+    json = Request.fetch("list", params)
     {:ok, map} = JSON.decode(json)
     %{"success" => true, "currencies" => currencies} = map
     {:ok, currencies}
