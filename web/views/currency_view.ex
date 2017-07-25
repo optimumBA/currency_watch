e51c36f0226166f12021ee7383fe08f32d37d4e1
Almir Sarajčić <almir.sarajcic@icloud.com>
2017-07-25 10:44:12 +0200

    Add flag_url

--- web/views/currency_view.ex
+++ web/views/currency_view.ex
@@ -1,5 +1,6 @@
 defmodule CurrencyWatch.CurrencyView do
   use CurrencyWatch.Web, :view
+  import CurrencyWatch.Router.Helpers, [:static_url]
 
   def render("index.json", %{currencies: currencies}) do
     render_many(currencies, __MODULE__, "currency.json")
@@ -10,6 +11,11 @@ defmodule CurrencyWatch.CurrencyView do
       id: currency.id,
       code: currency.code,
       name: currency.name,
+      flag_url: flag_url(currency),
     }
+  end
+
+  def flag_url(currency) do
+    static_url(CurrencyWatch.Endpoint, "/images/flags/#{currency.flag}.png")
   end
 end
