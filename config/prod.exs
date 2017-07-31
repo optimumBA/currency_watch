50aecbeec8d4b5e8d4ecd567815e09a59e6166c2
Almir Sarajčić <almir.sarajcic@icloud.com>
2017-07-31 09:03:34 +0200

    Setup for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :currency_watch, CurrencyWatch.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "cw-s-eu-4692.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -55,6 +57,12 @@ config :logger, level: :info
 #
 #     config :currency_watch, CurrencyWatch.Endpoint, server: true
 #
+
+config :currency_watch, CurrencyWatch.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
