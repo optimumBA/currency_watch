fce31e46f1ad0a5761eaebfe50d5efad99df1c88
Almir Sarajčić <almir.sarajcic@icloud.com>
2017-08-01 22:39:21 +0200

    Add task for exchange rates cleanup

---
+++ test/mix/tasks/currency_watch.rates.cleanup_test.exs
@@ -0,0 +1,31 @@
+defmodule Mix.Tasks.CurrencyWatch.Rates.CleanupTest do
+  use CurrencyWatch.ModelCase
+
+  alias CurrencyWatch.ExchangeRate
+  alias CurrencyWatch.Repo
+
+  describe "run" do
+    test "doesn't call currency API when no new currencies" do
+      currency = insert_currency()
+      timestamp = DateTime.to_unix(DateTime.utc_now)
+
+      {:ok, datetime} = DateTime.from_unix(timestamp - 2 * 24 * 60 * 60 - 1)
+      currency
+      |> Ecto.build_assoc(:exchange_rates, value: 0.858946, inserted_at: datetime)
+      |> Repo.insert
+
+      {:ok, datetime} = DateTime.from_unix(timestamp - 2 * 24 * 60 * 60 + 1)
+      {:ok, new_exchange_rate} = currency
+      |> Ecto.build_assoc(:exchange_rates, value: 0.858946, inserted_at: datetime)
+      |> Repo.insert
+
+      Mix.Tasks.CurrencyWatch.Rates.Cleanup.run("")
+
+      exchange_rates = Repo.all(ExchangeRate)
+      exchange_rate = Enum.at(exchange_rates, 0)
+
+      assert length(exchange_rates) == 1
+      assert new_exchange_rate.id == exchange_rate.id
+    end
+  end
+end
