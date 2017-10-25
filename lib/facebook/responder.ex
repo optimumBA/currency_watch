7906bae247adb06272142114b2b3ff4f94d18408
Almir Sarajčić <almir.sarajcic@icloud.com>
2017-10-25 20:35:51 +0200

    Add more responder functions

--- lib/facebook/responder.ex
+++ lib/facebook/responder.ex
@@ -4,13 +4,24 @@ defmodule Facebook.Responder do
   @endpoint "https://graph.facebook.com/v2.6/me/messages"
   @access_token Application.get_env(:currency_watch, :facebook)[:access_token]
 
-  def mark_as_seen(sender_id) do
-    body = %{
+  def send_message(recipient_id, text) do
+    respond(recipient_id, %{message: %{text: text}})
+  end
+
+  def mark_as_seen(recipient_id) do
+    respond(recipient_id, %{sender_action: "mark_seen"})
+  end
+
+  def show_typing_indicator(recipient_id) do
+    respond(recipient_id, %{sender_action: "typing_on"})
+  end
+
+  defp respond(recipient_id, data) do
+    body = Map.merge(data, %{
       recipient: %{
-        id: sender_id,
+        id: recipient_id,
       },
-      sender_action: "mark_seen",
-    }
+    })
 
     Request.new(:post, @endpoint, body, %{access_token: @access_token}, [{"Content-Type", "application/json"}])
     |> Client.send()
--- test/facebook/responder_test.exs
+++ test/facebook/responder_test.exs
@@ -4,6 +4,30 @@ defmodule Facebook.ResponderTest do
   alias CurrencyWatch.Client.InMemoryClient
   alias CurrencyWatch.{Request, Response}
 
+  test "sends message" do
+    response = %Response{status: :ok, body: %{
+      "recipient_id" => "1008372609250235",
+      "message_id" => "mid.1456970487936:c34767dfe57ee6e339",
+    }}
+    InMemoryClient.push(response)
+
+    assert Facebook.Responder.send_message(1008372609250235, "message text") == response
+    assert %Request{
+      method: :post,
+      endpoint: "https://graph.facebook.com/v2.6/me/messages",
+      params: %{access_token: _},
+      body: %{
+        recipient: %{
+          id: 1008372609250235,
+        },
+        message: %{
+          text: "message text",
+        },
+      },
+      headers: [{"Content-Type", "application/json"}]
+    } = InMemoryClient.pop()
+  end
+
   test "marks message as seen" do
     response = %Response{status: :ok, body: %{
       "recipient_id" => "1340752069386539"
@@ -11,7 +35,6 @@ defmodule Facebook.ResponderTest do
     InMemoryClient.push(response)
 
     assert Facebook.Responder.mark_as_seen(1340752069386539) == response
-
     assert %Request{
       method: :post,
       endpoint: "https://graph.facebook.com/v2.6/me/messages",
@@ -21,6 +44,27 @@ defmodule Facebook.ResponderTest do
           id: 1340752069386539,
         },
         sender_action: "mark_seen",
+      },
+      headers: [{"Content-Type", "application/json"}]
+    } = InMemoryClient.pop()
+  end
+
+  test "shows typing indicator" do
+    response = %Response{status: :ok, body: %{
+      "recipient_id" => "1340752069386539"
+    }}
+    InMemoryClient.push(response)
+
+    assert Facebook.Responder.show_typing_indicator(1340752069386539) == response
+    assert %Request{
+      method: :post,
+      endpoint: "https://graph.facebook.com/v2.6/me/messages",
+      params: %{access_token: _},
+      body: %{
+        recipient: %{
+          id: 1340752069386539,
+        },
+        sender_action: "typing_on",
       },
       headers: [{"Content-Type", "application/json"}]
     } = InMemoryClient.pop()
