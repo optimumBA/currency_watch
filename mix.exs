12f50a997d5824bf6b9091d1a3a52779df784b8a
Almir Sarajčić <almir.sarajcic@icloud.com>
2019-03-14 14:09:00 +0100

    Fix server issue

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule CurrencyWatch.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
+     {:plug_cowboy, "~> 1.0"},
      {:junit_formatter, "~> 1.1", only: :test},
      {:json, "~> 1.0"}]
   end
