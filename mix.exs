22da6f707db52df74effba85e9dc32a356962ea7
Almir Sarajčić <almir.sarajcic@icloud.com>
2019-11-22 13:20:41 +0100

    Fix DB connection issue

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule CurrencyWatch.MixProject do
   def application do
     [
       mod: {CurrencyWatch.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 
