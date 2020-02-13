4713f46aed8060f4d5fd37b161cac8f4225f8fae
Almir Sarajčić <almir.sarajcic@icloud.com>
2020-02-13 17:38:30 +0100

    Fix missing HOST in review apps

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# Set HOST env variable for Heroku review apps
+if [ -z "$HOST" ] && [ -n "$HEROKU_APP_NAME" ]; then
+  export HOST=$HEROKU_APP_NAME.herokuapp.com
+fi
+
 bin/currency_watch eval 'CurrencyWatch.Release.migrate'
 
 exec "$@"
